993ec977472f0579d9629c241c1503311c3949ed
Jacob Perkins <jacob.a.perkins@gmail.com>
2011-02-27 13:24:41 -0600

    handle for a filesytem file object should be an attr_accessor so, when all else fails, one can have access to the file handle

--- lib/swineherd/filesystem/localfilesystem.rb
+++ lib/swineherd/filesystem/localfilesystem.rb
@@ -1,13 +1,13 @@
 require 'fileutils'
 module Swineherd
-  
+
   class LocalFileSystem
 
     include Swineherd::BaseFileSystem
 
     def initialize *args
     end
-    
+
     def open path, mode="r", &blk
       return LocalFile.new path, mode, &blk
     end
@@ -15,23 +15,23 @@ module Swineherd
     def rm path
       FileUtils.rm_r path
     end
-    
+
     def exists? path
       File.exists?(path)
     end
-    
+
     def mv srcpath, dstpath
       FileUtils.mv(srcpath,dstpath)
     end
-    
+
     def cp srcpath, dstpath
       FileUtils.cp_r(srcpath,dstpath)
     end
-    
+
     def mkpath path
       FileUtils.mkpath path
     end
-    
+
     def type path
       case
       when File.symlink?(path) then
@@ -39,45 +39,45 @@ module Swineherd
       when File.directory?(path) then
         return "directory"
       when File.file?(path) then
-          return "file"        
+          return "file"
       end
       "unknown"
     end
-    
+
     def entries dirpath
       return unless (type(dirpath) == "directory")
       Dir.entries(dirpath)
     end
-    
+
     class LocalFile
-      attr_accessor :path, :scheme, :mode
-      
+      attr_accessor :path, :scheme, :handle, :mode
+
       def initialize path, mode="r", &blk
         @path   = path
         @mode   = mode
         @handle = File.open(path,mode,&blk)
       end
-      
+
       def open path, mode="r", &blk
         initialize(path,mode,&blk)
       end
-      
+
       def read
         @handle.read
       end
-      
+
       def readline
         @handle.gets
       end
-      
+
       def write string
         @handle.write(string)
       end
-      
+
       def close
         @handle.close
       end
     end
-    
+
   end
 end
